c0d68aeb2e087d81452b927d7c2a5f2f9b77463a
Coda Hale <coda.hale@gmail.com>
2015-03-26 08:39:04 -0700

    Hash is no longer unstable!
    (yaaaay)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! Sketchy is a Rust library of probabilistic data structures, useful for measuring large or
 //! unbounded streams of data by trading some accuracy for a whole lot of efficiency.
 
-#![feature(collections, core, hash)]
+#![feature(collections, core)]
 
 extern crate rand;
 
