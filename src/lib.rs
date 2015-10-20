4be095aeb9843d88784235ffeb1ea1d2d658818f
Will <will@glozer.net>
2015-10-20 11:22:56 +0900

    Collections & core are stable, iter_arith isn't.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! Sketchy is a Rust library of probabilistic data structures, useful for measuring large or
 //! unbounded streams of data by trading some accuracy for a whole lot of efficiency.
 
-#![feature(collections, core)]
+#![feature(iter_arith)]
 
 extern crate num;
 extern crate rand;
